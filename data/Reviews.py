6e24287a78520ef6b53c4ee3885285b3649f61ce
Dana <pyatnichenko05@mail.ru>
2022-05-10 03:16:01 +0500

    Commit 1

---
+++ data/Reviews.py
@@ -0,0 +1,15 @@
+import sqlalchemy
+from sqlalchemy import orm
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from .db_session import SqlAlchemyBase
+
+
+class Review(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'reviews'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    place = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    img = sqlalchemy.Column(sqlalchemy.String, nullable=True)
